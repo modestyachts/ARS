95c4c818dc9d18c421d27e02e4885abdf1e3ddcc
bsword <horiamania.gmail.com>
2018-03-19 17:46:50 -0700

    make HalfCheetah default task

--- code/ars.py
+++ code/ars.py
@@ -386,23 +386,24 @@ def run_ars(params):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env_name', type=str, default='Humanoid-v1')
+    parser.add_argument('--env_name', type=str, default='HalfCheetah-v1')
     parser.add_argument('--n_iter', '-n', type=int, default=1000)
-    parser.add_argument('--n_directions', '-nd', type=int, default=230)
-    parser.add_argument('--deltas_used', '-du', type=int, default=230)
+    parser.add_argument('--n_directions', '-nd', type=int, default=8)
+    parser.add_argument('--deltas_used', '-du', type=int, default=8)
     parser.add_argument('--step_size', '-s', type=float, default=0.02)
-    parser.add_argument('--delta_std', '-std', type=float, default=.0075)
-    parser.add_argument('--n_workers', '-e', type=int, default=48)
+    parser.add_argument('--delta_std', '-std', type=float, default=.03)
+    parser.add_argument('--n_workers', '-e', type=int, default=18)
     parser.add_argument('--rollout_length', '-r', type=int, default=1000)
 
-    # for Swimmer-v1 and HalfCheetah-v1 use shift = 'consant_zero'
-    # for Hopper-v1, Walker2d-v1, and Ant-v1 use shift = 'constant_one'
-    parser.add_argument('--shift', type=float, default=5)
+    # for Swimmer-v1 and HalfCheetah-v1 use shift = 0
+    # for Hopper-v1, Walker2d-v1, and Ant-v1 use shift = 1
+    # for Humanoid-v1 used shift = 5
+    parser.add_argument('--shift', type=float, default=0)
     parser.add_argument('--seed', type=int, default=237)
     parser.add_argument('--policy_type', type=str, default='linear')
     parser.add_argument('--dir_path', type=str, default='data')
 
-    # for ARS v1 use filter = 'NoFilter'
+    # for ARS V1 use filter = 'NoFilter'
     parser.add_argument('--filter', type=str, default='MeanStdFilter')
 
     local_ip = socket.gethostbyname(socket.gethostname())
