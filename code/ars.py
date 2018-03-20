af12cc705313ff7c766e333c71698346f74d2135
bsword <horiamania.gmail.com>
2018-03-19 17:40:44 -0700

    split the shared noise code into a separate file

--- code/ars.py
+++ code/ars.py
@@ -3,9 +3,6 @@ Parallel implementation of the Augmented Random Search method.
 Horia Mania --- hmania@berkeley.edu
 Aurelia Guy
 Benjamin Recht 
-
-
-
 '''
 
 import parser
@@ -19,37 +16,7 @@ import utils
 import optimizers
 from policies import *
 import socket
-
-@ray.remote
-def create_shared_noise():
-    """
-    Create a large array of noise to be shared by all workers. Used 
-    for avoiding the communication of the random perturbations delta.
-    """
-
-    seed = 12345
-    count = 250000000
-    noise = np.random.RandomState(seed).randn(count).astype(np.float64)
-    return noise
-
-
-class SharedNoiseTable(object):
-    def __init__(self, noise, seed = 11):
-
-        self.rg = np.random.RandomState(seed)
-        self.noise = noise
-        assert self.noise.dtype == np.float64
-
-    def get(self, i, dim):
-        return self.noise[i:i + dim]
-
-    def sample_index(self, dim):
-        return self.rg.randint(0, len(self.noise) - dim + 1)
-
-    def get_delta(self, dim):
-        idx = self.sample_index(dim)
-        return idx, self.get(idx, dim)
-
+from shared_noise import *
 
 @ray.remote
 class Worker(object):
